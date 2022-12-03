cfb9f80daf0163ced8042e9c580faed16afde021
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-12-03 18:49:04 -0300

    p2 done

--- 2022/d3/p2.rb
+++ 2022/d3/p2.rb
@@ -1,38 +1,27 @@
 def main
-    sum = 0
-    group = []
-    File.open("input.txt").each do |line|
-      if group.length == 6
-        b1,b2 = findBadge(group[0..2]), findBadge(group[3..6])
-        p b1,b2 
-        sum += parse(b1) + parse(b2)
-        group.clear
-      else
-        group << line.chop
-      end
-    end
+  sum = 0
+  group = []
+  File.open('input.txt').each do |line|
+    group << line.chop
+    next if group.length != 3
+
+    b1 = findBadge(group[0..2])
+    sum += parse(b1)
+    group.clear
+  end
   p sum
 end
 
 def findBadge(g)
-  r = ''
-  g[0].each_char do |char|
-    achou = false
-    if g[1].include? char 
-      achou = true
-    end
-    if g[2].include? char 
-      return char if achou
-    end
+  g[0].each_char do |c|
+    return c if g[1].include? c and g[2].include? c
   end
 end
 
 def parse(char)
-  unless ('A'..'Z').include? char 
-    return char.ord - 96
-  else 
-    return char.ord - 38
-  end
+  return char.ord - 96 unless ('A'..'Z').include? char
+
+  char.ord - 38
 end
 
 main
