c75c38cbb226aa45071a414a97cbcabb9b9dd574
lucasdellatorre <lucasdellatorre222@gmail.com>
2022-12-03 19:37:10 -0300

    refactor p1

--- 2022/d3/p1.rb
+++ 2022/d3/p1.rb
@@ -1,18 +1,14 @@
-def main
-    common = ''
-    sum = 0
-    File.open("input.txt").each do |line|
-      r1,r2 = line[0, line.length / 2], line[line.length / 2, line.length]
-      r1.each_char do |char|
-        common = char if r2.include? char
-      end
-      unless ('A'..'Z').include? common 
-        sum += common.ord - 96
-      else 
-        sum += common.ord - 38
-      end
-    end
-    p sum
+common = ''
+sum = 0
+File.open('input.txt').each do |line|
+  r1,r2 = line.chars.each_slice(line.length / 2).map(&:join)
+  r1.each_char do |char|
+    common = char if r2.include? char
+  end
+  sum += if ('A'..'Z').include? common
+           common.ord - 38
+         else
+           common.ord - 96
+         end
 end
-
-main
+p sum
