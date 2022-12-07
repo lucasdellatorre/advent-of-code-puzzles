af788f8ad06f07537822580aa450451e393ef0e3
Lucas Dellatorre <lucasdellatorre222@gmail.com>
2022-12-06 22:48:09 -0300

    Update p1.rb

--- 2022/d6/p1.rb
+++ 2022/d6/p1.rb
@@ -2,7 +2,6 @@ require 'set'
 
 s = File.read('input.txt').chomp
 N = s.length
-pos = 0
 (0..N - 1).each do |i|
   set = Set.new
   (i..N).each do |j|
@@ -10,7 +9,7 @@ pos = 0
 
     set << s[j]
     if set.length == 4
-      puts j + 1
+      puts j + 1 # +1 because we are not counting from zero
       break
     end
   end
