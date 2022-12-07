5fed47055462286ef376683b361c5a2a74dea6b7
lucasdellatorreLIS <lucas.freitas@pucrs.br>
2022-12-06 22:39:44 -0300

    removing r

--- 2022/d6/p1.rb
+++ 2022/d6/p1.rb
@@ -2,12 +2,10 @@ require 'set'
 
 s = File.read('input.txt').chomp
 N = s.length
-r = 0
 pos = 0
 (0..N - 1).each do |i|
   set = Set.new
   (i..N).each do |j|
-    r += 1
     break if set.include? s[j]
 
     set << s[j]
@@ -18,5 +16,4 @@ pos = 0
   end
   break if set.length == 4
 end
-p r
 p pos + 1
